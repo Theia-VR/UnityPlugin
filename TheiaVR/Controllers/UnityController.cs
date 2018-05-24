1cca8686b8a383444dfe5ceac11994aa02fa3457
Pourfex <alexis.delforges@gmail.com>
2018-05-24 18:09:05 +0200

    instantiate 25 cubes + moving first one

--- TheiaVR/Controllers/UnityController.cs
+++ TheiaVR/Controllers/UnityController.cs
@@ -8,6 +8,9 @@ namespace TheiaVR.Controllers
     {
         public GameObject vertex;
 
+        private static int numberOfVertexs = 15;
+        private GameObject[] vertexs;
+
         void OnEnable()
         {
             Messages.Log("Enabling TheiaVR plugin");
@@ -19,15 +22,33 @@ namespace TheiaVR.Controllers
             try
             {
                 StreamController.GetInstance().Start("127.0.0.1", 11000);
-                //BodyController.GetInstance().Start(vertex);
+                startBodyController();   
             }
             catch(Exception vException)
             {
                 Messages.Log("<color=red>" + vException.Message + "</color>");
             }
+            
             Messages.Log("TheiaVR correctly started");
         }
-        
+
+        private void startBodyController()
+        {
+            Messages.Log("Body Controller started");
+            vertexs = new GameObject[numberOfVertexs];
+            for (int i = 0; i < numberOfVertexs; i++)
+            {
+                vertexs[i] = Instantiate(vertex, new Vector3((float)i, (float)i, (float)i), Quaternion.identity);
+            }
+            Messages.Log("BodyController : " + vertexs.ToString());
+            Messages.Log("BodyController : Instantiate all vertexs");
+        }
+
+        void Update()
+        {
+            vertexs[0].transform.GetComponent<Rigidbody>().AddForce(transform.forward * 2);
+        }
+
         void OnApplicationQuit()
         {
             Messages.Log("Stopping TheiaVR plugin");
