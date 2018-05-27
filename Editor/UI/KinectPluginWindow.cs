6c42387f513e174328f4207a1e516bb9b5521d7a
batnyu <vrignaudbaptiste@gmail.com>
2018-05-27 22:55:21 +0200

    fix(ui): removing useless import

--- Editor/UI/KinectPluginWindow.cs
+++ Editor/UI/KinectPluginWindow.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Text.RegularExpressions;
-using NUnit.Framework.Constraints;
 using TheiaVR.Controllers;
 using TheiaVR.Helpers;
 using UnityEngine;
