9805df68026c7bc35d6e0633146f1202d893cd6a
batnyu <vrignaudbaptiste@gmail.com>
2018-06-03 19:05:09 +0200

    feat(ui): multi-kinect

---
+++ TheiaVR/Editor/NetworkConfig.cs
@@ -0,0 +1,24 @@
+﻿namespace TheiaVR.Editor
+{
+    public class NetworkConfig
+    {
+        public NetworkConfig(string ipAddress, int cloudPort, int skelPort, bool enableCloud, bool enableSkel)
+        {
+            this.IpAddress = ipAddress;
+            this.CloudPort = cloudPort;
+            this.SkelPort = skelPort;
+            this.EnableCloud = enableCloud;
+            this.EnableSkel = enableSkel;
+        }
+        
+        public string IpAddress { get; set; }
+
+        public int CloudPort { get; set; }
+
+        public int SkelPort { get; set; }
+
+        public bool EnableCloud { get; set; }
+
+        public bool EnableSkel { get; set; }
+    }
+}
