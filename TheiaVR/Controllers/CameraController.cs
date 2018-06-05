8320116a7fb157b513b604fb430f4c1be03a3c0a
pourfex <alexis.delforges@gmail.com>
2018-06-05 17:48:04 +0200

    KeyBoard and mouse, miss mouse Y axis

---
+++ TheiaVR/Controllers/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    Camera mainCamera;
+    float speed;
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main;
+        speed = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 rotation;
+        Vector3 move;
+
+        move = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            move = new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            move = new Vector3(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            move = new Vector3(0, 0, -speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            move = new Vector3(0, 0, speed * Time.deltaTime);
+        }
+
+        float mouseInputX = Input.GetAxis("Mouse X");
+        float mouseInputY = Input.GetAxis("Mouse Y");
+        //rotation = new Vector3(-mouseInputY, mouseInputX, 0);
+        rotation = new Vector3(0, mouseInputX, 0);
+
+        mainCamera.transform.Translate(move);
+        mainCamera.transform.Rotate(rotation);
+
+    }
+}
